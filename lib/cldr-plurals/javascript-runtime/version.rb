ecb8471fecf0619fe2d91e491228975952f91364
Cameron Dutro <cameron@lumoslabs.com>
2014-09-30 10:41:17 -0700

    Initial commit

---
+++ lib/cldr-plurals/javascript-runtime/version.rb
@@ -0,0 +1,5 @@
+# encoding: UTF-8
+
+module CldrPlurals
+  JAVASCRIPT_RUNTIME_VERSION = '1.0.0'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+require 'rspec'
+require 'pry-nav'
+require 'cldr-plurals/javascript_runtime'
+
+RSpec.configure do |config|
+  config.mock_with :rr
+end
